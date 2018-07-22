92c86e02a0babe3ba271ddc25d3c70d0bd247256
Aakash Dave <aakashdave@Aakashs-MacBook-Air.local>
2018-07-21 17:14:33 -0700

    first commit

---
+++ firstprogram.py
@@ -0,0 +1,21 @@
+x = 42
+y =  31
+print('Hello world {0} and {0} and {1}'.format(x,y))
+arr = [[12,13,14,15],[16,17,18,19],[1,2,3,4],[5,6,7,8]]
+
+if x==42 :
+    print(1)
+    print(2)
+
+print(3)
+
+
+for currentrow in arr :
+    for currentCol in currentrow :
+        print(currentCol)
+
+
+arr1 = ['akash','sachin','pratik']
+print('{0}'.format(arr[3][0]))
+print(arr1)
+print('\n'.join(arr1))
