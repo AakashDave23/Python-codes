104e48b73ac02d3f3d65e962a3b6a1a6343bf91a
Aakash Dave <aakashdave@Aakashs-MacBook-Air.local>
2018-07-22 17:53:57 -0700

    fifth commit

---
+++ fifthprogram.py
@@ -0,0 +1,13 @@
+# Hec and Binary
+
+x = 0x0a
+y = 0x02
+
+print(x)
+
+z = x & y
+
+print ('{}'.format(z))
+
+print ('x is {0:02x}, y is {1:02x}, z is {2:02x}'.format(x,y,z))
+print (' is {0:08b}, y is {1:08b}, z is {2:08b}'.format(x,y,z))
